8f1d9c4a335c0916d67587d98ebb5537f113df38
TapSel0 <79279863+TapSel0@users.noreply.github.com>
2023-10-25 19:58:01 +0700

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+board = list(range(9))
+count_of_cells = 3
+print_dashes = lambda: print("-" * 13)
+print_spaces = lambda: print(" " * 14)
+
+counter = 0
+is_win = False
+win_cords = ((0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2), (0, 1, 2))
